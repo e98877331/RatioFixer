968551c4518bb1ada39875d75e6393d04375f409
ChengYan Yang <e98877331@gmail.com>
2013-06-14 01:03:17 +0800

    add onInitialize in RationActivity, all initialization should be done therer, such as Ratiofixer.setVirtualSize

--- src/wcm/ytwhyc/ratiofixer/RatioActivity.java
+++ src/wcm/ytwhyc/ratiofixer/RatioActivity.java
@@ -1,11 +1,8 @@
 package wcm.ytwhyc.ratiofixer;
 
-
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Point;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.Gravity;
@@ -15,68 +12,76 @@ import android.widget.FrameLayout.LayoutParams;
 public class RatioActivity extends Activity {
 
 	RatioRelativeLayout ratioLayout;
-	
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-	// TODO Auto-generated method stub
-	super.onCreate(savedInstanceState);
-	
-	int x,y;
-	WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
-	Display display = wm.getDefaultDisplay();
-	
-//    if(Build.VERSION.SDK_INT>Build.VERSION_CODES.HONEYCOMB){                   
-//        Point size = new Point();
-//        display.getSize(size);
-//        x = size.x;
-//        y = size.y;
-//    }
-//    else{
-        x = display.getWidth();  // deprecated
-        y = display.getHeight();
-//    }
 
-	
-	int statusBarHeight = getStatusBarHeight();
-	
-	RatioFixer.initialize(x, y - statusBarHeight);
-	
-	ratioLayout = new RatioRelativeLayout(this);
-	ratioLayout.post(new Runnable() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
 
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-onLayoutCreated();
-		}
-	});
-	ratioLayout.setBackgroundColor(Color.GRAY);
+		onInitialize();
+		
+		int x, y;
+		WindowManager wm = (WindowManager) this
+				.getSystemService(Context.WINDOW_SERVICE);
+		Display display = wm.getDefaultDisplay();
 
-	LayoutParams layoutParams = new LayoutParams(
-			android.view.ViewGroup.LayoutParams.MATCH_PARENT,
-			android.view.ViewGroup.LayoutParams.MATCH_PARENT);
-	layoutParams.gravity = Gravity.CENTER;
-	setContentView(ratioLayout, layoutParams);
-	getWindow().getDecorView().findViewById(android.R.id.content).setBackgroundColor(Color.RED);
-}
-	
-public int getStatusBarHeight() {
-	  int result = 0;
-	  int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-	  if (resourceId > 0) {
-	      result = getResources().getDimensionPixelSize(resourceId);
-	  }
-	  return result;
+		// if(Build.VERSION.SDK_INT>Build.VERSION_CODES.HONEYCOMB){
+		// Point size = new Point();
+		// display.getSize(size);
+		// x = size.x;
+		// y = size.y;
+		// }
+		// else{
+		x = display.getWidth(); // deprecated
+		y = display.getHeight();
+		// }
+
+		int statusBarHeight = getStatusBarHeight();
+
+		RatioFixer.initialize(x, y - statusBarHeight);
+
+		ratioLayout = new RatioRelativeLayout(this);
+		ratioLayout.post(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				onLayoutCreated();
+			}
+		});
+		ratioLayout.setBackgroundColor(Color.GRAY);
+
+		LayoutParams layoutParams = new LayoutParams(
+				android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+				android.view.ViewGroup.LayoutParams.MATCH_PARENT);
+		layoutParams.gravity = Gravity.CENTER;
+		setContentView(ratioLayout, layoutParams);
+//		getWindow().getDecorView().findViewById(android.R.id.content)
+//				.setBackgroundColor(Color.RED);
 	}
 
-public void onLayoutCreated()
-{
-	
-}
+	public int getStatusBarHeight() {
+		int result = 0;
+		int resourceId = getResources().getIdentifier("status_bar_height",
+				"dimen", "android");
+		if (resourceId > 0) {
+			result = getResources().getDimensionPixelSize(resourceId);
+		}
+		return result;
+	}
 
-public RatioRelativeLayout getMainLayout()
-{
-	return ratioLayout;
-}
+	
+	public void onInitialize()
+	{
+		
+	}
+	
+	public void onLayoutCreated() {
+
+	}
+	
+	public RatioRelativeLayout getMainLayout() {
+		return ratioLayout;
+	}
 
 }
